1c4c7f218c625f29ff1e17a0131f2794ee3b07bb
Devon Bernard <dwbproductions@hotmail.com>
2015-04-16 14:43:56 -0400

    Revisioned app for metro framework

---
+++ DeveloperDesktop/DeveloperDesktop/DeveloperDesktop.Windows/Controllers/WindowsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace DeveloperDesktop.Controllers
+{
+    public class WindowsController
+    {
+        int height = 250, width = 500, count = 0;
+        public List<WebView> windowList = new List<WebView>();
+        public List<Button> buttonList = new List<Button>();
+
+        public  int createWindow(string url)
+        {
+            WebView window = new WebView();
+            window.Height = height;
+            window.Width = width;
+            int row = (count%3);
+            int col = count / 3;
+            window.Margin = new Thickness(row*(width)+(row+1)*25, col*height+(col+1)*25, 0, 0);
+            window.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+            window.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+            window.Source = new Uri(url);
+            window.DataContext = count;
+            windowList.Add(window);
+
+            Button closeButton = new Button();
+            closeButton.Content = "X";
+            closeButton.Margin = new Thickness((row)*width + (row + 1) * 25, col * height + (col + 1) * 25, 0, 0);
+            closeButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+            closeButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+            closeButton.DataContext = count;
+            closeButton.Tapped += closeWindow;
+            buttonList.Add(closeButton);
+
+            count++;
+            return count-1;
+        }
+
+        public void closeWindow(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            {
+                Button button = (Button)sender;
+                int index = (int)button.DataContext;
+                var warning = new Windows.UI.Popups.MessageDialog("DELETE: " + index);
+                warning.ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                var warning = new Windows.UI.Popups.MessageDialog("Can't delete item");
+                warning.ShowAsync();
+            }
+            
+        }
+
+        public WindowsController()
+        {
+            // TODO: Complete member initialization
+
+        }
+    }
+}
---
+++ DeveloperDesktop/DeveloperDesktop/DeveloperDesktop.Windows/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using DeveloperDesktop.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DeveloperDesktop
+{
+    
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// 
+    public sealed partial class MainPage : Page
+    {
+        WindowsController windowsController = new WindowsController();
+        public MainPage mainPage;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            mainPage = this;
+            openWindows();
+        }
+        private void OpenContextMenu(object sender, RightTappedRoutedEventArgs e)
+        {
+            //var message = new Windows.UI.Popups.MessageDialog("RIGHT CLICK BITCH!");
+            //message.ShowAsync();
+        }
+
+        private void openWindows()
+        {
+            List<string> urls = new List<string>();
+            urls.Add("http://www.youtube.com");
+            urls.Add("http://www.twitter.com");
+            urls.Add("http://www.github.com");
+            for (int i = 0; i < urls.Count; i++)
+            {
+                int window_index = windowsController.createWindow(urls[i]);
+                RootGrid.Children.Add(windowsController.windowList[window_index]);
+                RootGrid.Children.Add(windowsController.buttonList[window_index]);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = this.url_bar.Text;
+            try
+            {
+                Uri uri = new Uri(url);
+                int window_index = windowsController.createWindow(url);
+                RootGrid.Children.Add(windowsController.windowList[window_index]);
+                RootGrid.Children.Add(windowsController.buttonList[window_index]);
+                this.url_bar.Text = "";
+            }
+            catch (Exception ex)
+            {
+                var warning = new Windows.UI.Popups.MessageDialog("Invalid URL");
+                warning.ShowAsync();
+            }
+            
+        }
+    }
+}
